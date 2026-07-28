5de1bbf160994a88a5840c1a13bfbb4c7bab042d
tara <taralamia69@gmail.com>
2026-07-28 22:37:24 +0600

    feat: add request logging middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import healthRouter from "./routes/health.route";
-
+import {logger} from "./middlewares/logger.middleware";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(logger);
 app.use("/api/v1", healthRouter);
 
 export default app;
